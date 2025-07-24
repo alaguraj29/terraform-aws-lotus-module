21ae242fc61266fd0252cde81f8b713128ef0e3c
alaguraj29 <alaguraj29@gmail.com>
2025-07-24 20:12:19 +0530

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -1,6 +1,24 @@
-variable "amiid" {}
-variable "instancetype" {}
-variable "keyname" {}
-variable "tag" {}
-variable "cidr" {}
-variable "sgname" {}
+variable "amiid" {
+  type = string
+}
+
+variable "instancetype" {
+  type = string
+}
+
+variable "keyname" {
+  type = string
+}
+
+variable "tag" {
+  type = string
+}
+
+variable "cidr" {
+  type = list(string)
+}
+
+variable "sgname" {
+  type = string
+}
+
